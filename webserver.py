621a999ae5c02d9408d3a4e9aee146fd405b2a74
Kevin Chen <79040821+kc80447@users.noreply.github.com>
2022-07-26 15:13:21 -0700

    Add files via upload
    Added an add new task button

--- webserver.py
+++ webserver.py
@@ -4,26 +4,39 @@ tasklist = ['Task 1', 'Task 2', 'Task 3']
 
 class requestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
-        self.send_header('content-type', 'text/html')
-        self.end_headers()
-
-        output = ''
-        output +='<html><body>'
-        output+= '<h1>Task List </h1>'
-        for task in tasklist:
-          output += task
-          output += '</br>'
-
-        output += '</body></html>'
-        self.wfile.write(output.encode())
-
+        if self.path.endswith('/tasklist'):
+          
+          self.send_response(200)
+          self.send_header('content-type', 'text/html')
+          self.end_headers()
       
+          output = ''
+          output +='<html><body>'
+          output+= '<h1>Task List </h1>'
+          output+='<h3><a href = "/tasklist/new"> Add New Task</a></h3>'
+          for task in tasklist:
+            output += task
+            output += '</br>'
+      
+          output += '</body></html>'
+          self.wfile.write(output.encode())
 
+        if self.path.endswith('/new'):
+          self.send_response(200)
+          self.send_header('content-type', 'text/html')
+          self.end_headers()
+
+          output = ''
+          output += '<html><body>' 
+          output +='<h1>Add New Task </h1>'
+          output +='<form method = "Post" enctype = "multipart/form-data" action = "/tasklist/new">'
+          output += '<input name = "task" type = "text" placeholder = "Add new task">'
+          output += '<input type = "submit" value = "Add">'
+          output += '</form>'
+          output += '</body></html>'
+          self.wfile.write(output.encode())
+          
         
-
-
-      
 
 
 def main():
